3ea1aa01e82d590d4f90bbf64f07fdb8c02aff37
Joseph Wan <joseph.h.wan@gmail.com>
2023-06-28 21:12:34 -0500

    feat: add links to resume, linkedin, and github

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,19 +1,40 @@
 import profilePic from "./img/profile_pic.jpeg"
+import RightArrowIcon from "./icons/RightArrowIcon"
+import LinkedInIcon from "./icons/LinkedInIcon"
+import GitHubIcon from "./icons/GitHubIcon"
 
 export default function Profile() {
   return (
-    <div className="max-w-xl">
-      <h2 className="text-3xl font-bold sm:text-4xl">Joseph Wan | Software Developer</h2>
-      <div className="flex">
-        <div className="w-1/2 mr-4 items-center">
-          <div className="mt-4 text-center">
-            <img className="rounded-full" src={ profilePic } alt="profile pic"/>
+    <div className="">
+      <div className="flex items-baseline">
+        <h2 className="text-3xl mr-4 font-bold sm:text-4xl">Joseph Wan | Software Developer</h2>
+        <a className="mr-4" href="https://www.linkedin.com/in/josephwan" target="_blank" rel="noreferrer">
+          <LinkedInIcon />
+        </a>
+        <a href="https://www.github.com/joseph1wan" target="_blank" rel="noreferrer">
+          <GitHubIcon />
+        </a>
+      </div>
+
+      <div className="flex w-2/3 my-6 items-center">
+        <div className="w-1/6 mr-8 items-center">
+          <div className="text-center">
+            <img className="border-2 border-white rounded-full" src={ profilePic } alt="profile pic"/>
           </div>
         </div>
 
-        <p className="mt-4 text-gray-300">
+        <p className="w-1/2 mr-4 text-gray-300">
           Innovative Software Developer with 6+ years of experience building robust software, web applications, services, and systems with a focus on streamlining inefficient processes and enabling fast, smart, and efficient work.
         </p>
+
+        <div className="group">
+          <a href="https://gitconnected.com/joseph1wan/resume" className="w-1/4 ml-4" target="_blank" rel="noreferrer">
+            <RightArrowIcon />
+          </a>
+          <div className="invisible text-xs text-gray-400 text-center group-hover:visible" role="tooltip">
+            Go to resume
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/icons/RightArrowIcon.tsx
@@ -0,0 +1,13 @@
+export default function RightArrowIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-20 w-20 text-pink-500 hover:text-pink-100"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+  )
+}
